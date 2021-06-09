e31e9c52414542ec6cd536f3dce098bc72c9a5f6
Yuna Tomida <ytomida.mmm@gmail.com>
2021-06-10 00:32:01 +0900

    足し算と引き算を処理できるように(ステップ2)

--- body/src/main.rs
+++ body/src/main.rs
@@ -8,7 +8,7 @@ use anyhow::{Context, Result};
     name = "rscc",
     version = "0.0.0",
     author = "Yuna Tomida",
-    about = "C compiler with Rust for study"
+    about = "学習のためにCコンパイラをRustで自作します。"
 )]
 
 struct Opts {
@@ -23,6 +23,7 @@ struct Opts {
 
 
 fn main() {
+    // 引数の処理
     let opts = Opts::parse();
     
     // 入力ファイルが指定されていない場合、パニック
@@ -31,28 +32,91 @@ fn main() {
         let reader = BufReader::new(f);
 
         for line in reader.lines() {
+            // このループ内でlineを式として解釈していく(以降のバージョンではこの部分の変数名はlineに統一する)
+            let line = line.unwrap();
+            
+            // 先頭から1文字ずつ値を読む。読み込んだ値が数値ならregに入れていき、そうでなければ演算子とみなす
+            // flagは、1つ前に演算子を読んだかを判定する。asmは出力する文字列(アセンブラコード)。
+            // 頭に+,-がくることについては許し、regが0なら計算を飛ばすという形で対処
+            let mut reg = 0;
+            let mut flag = false;
+            let mut asm = ".intel_syntax noprefix\n.globl main\nmain:\n".to_string();
+            let mut op_prev: char = ' ';
+            for c in line.as_str().chars() {
+                // 空白は無視
+                if c == ' ' {continue;}
 
-            let num_str = line.unwrap();
+                // 数値ならそれまで読んだ結果を1桁繰り上げてから足す
+                if c >= '0' && c <= '9' {
 
-            match num_str.parse::<i32>() {
-                Ok(num) => {
-                    // ナイーブにアセンブラを出力
-                    println!(".intel_syntax noprefix");
-                    println!(".global main");
-                    println!("main:");
-                    println!("  mov rax, {}", num);
-                    println!("  ret");
-                    println!("");
+                    reg = reg * 10 + (c.to_digit(10).unwrap() - '0'.to_digit(10).unwrap());
+                    if flag {flag = false}
 
-                }
-                Err(e) => {
-                    println!("something wrong when parsing to i32{:?}", e);
+                } else {
+                    // それ以外は演算子として扱う
+
+                    if flag {
+                        println!("\"{}\":演算子が連続しています。", c);
+                        return;
+                    }
+
+                    if reg == 0 {
+                        flag = true;
+                        continue;
+                    }
+
+                    match op_prev {
+                        '+' => {
+                            asm += format!("    add rax, {}\n", reg).as_str();
+                        }
+                        '-' => {
+                            asm += format!("    sub rax, {}\n", reg).as_str();
+                        }
+                        ' ' => {
+                            asm += format!("    mov rax, {}\n", reg).as_str();
+                        }
+                        _ => {
+                            println!("\"{}\":演算子として不正です。", op_prev);
+                            return;
+                        }
+                    }
+                    op_prev = c;
+                    // 読み込む整数のリセット及び演算子読みましたよフラグ
+                    reg = 0;
+                    flag = true;
                 }
             }
+            // 演算子で終わる場合、エラー
+            if flag {
+                println!("式が演算子で終了しています。");
+                return;
+            } 
+
+            if reg != 0 {
+                // あまり綺麗でないが、文末で最後の数字に対する計算を加える必要がある
+                match op_prev {
+                    '+' => {
+                        asm += format!("    add rax, {}\n", reg).as_str();
+                    }
+                    '-' => {
+                        asm += format!("    sub rax, {}\n", reg).as_str();
+                    }
+                    _ => {
+                        println!("\"{}\":演算子として不正です。", op_prev);
+                        return;
+                    }
+                }
+            }
+
+
+            // 最後に一気に書き込み
+            println!("{}", asm);
+
+            // 最初はひとまずソースの一行目のみを受け取るのみにしておく。
+            break;
         }
     } else {
-        println!("please specify an input file to compile.");
+        println!("ソースファイルを指定してください。");
     }
 
 }
-
