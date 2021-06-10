75c4b27d53e341f0556061f23308b06bb2df1a8d
Yuna Tomida <ytomida.mmm@gmail.com>
2021-06-10 22:42:27 +0900

    実行スクリプトのエスケープ($?)を修正し、コンテナ内のexit statusを正しく取得できるように。

--- exec-asm.sh
+++ exec-asm.sh
@@ -4,14 +4,13 @@ MOUNT_PATH=$(pwd)
 IMAGE_NAME=$1
 ASM_SRC=$2
 
-docker run --rm  -it\
+docker run --rm \
     -v $MOUNT_PATH:$MOUNT_PATH \
     --platform linux/amd64 \
     $IMAGE_NAME \
-    sh -c \
-    "
-    gcc -o ./tmp/tmp $MOUNT_PATH/$ASM_SRC;
+    /bin/bash -c \
+    "gcc -o ./tmp/tmp $MOUNT_PATH/$ASM_SRC;
     ./tmp/tmp;
+    echo \$?;
     rm tmp/tmp;
-    echo $?;
     "
