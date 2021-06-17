a45b8fd8966a27548584dbde240ade14d6cb6b14
Yuna Tomida <ytomida.mmm@gmail.com>
2021-06-17 18:54:25 +0900

    トークナイザのtokenize関数以外の関数を記述

--- step3/src/tokenizer.rs
+++ step3/src/tokenizer.rs
@@ -13,37 +13,56 @@ pub struct Token {
 	body: &str
 }
 
+// 入力文字列のトークナイズ
+pub fn tokenize(string: String) -> Token {
+	let mut head = Token::new();
+	head.next = None;
+
+	// 未完成
+	while  {
+
+	}
+
+}
+
+
+
+
 // 次のトークンが数字であることを期待して次のトークンを読む関数
-pub fn expect_number(token: Token) -> Restult<i32, ()>{}
+pub fn expect_number(token: Token) -> i32 {
+	if token.kind != Tokenkind::TK_NUM {
+		exit_eprintln!("数字であるべき位置で数字以外の文字\"{}\"が発見されました。", token.body);
+	}
+	let val = token.val;
+	// token.toNext();
+	val
+}
 
 // 期待する次のトークンを(文字列で)指定して読む関数(失敗するとexitする)
-pub fn expect(token: Token, op: String) -> Result<(), ()>{}
+pub fn expect(token: Token, op: String) {
+	if token.kind != Tokenkind::TK_RESERVED{
+		exit_eprintln!("予約されていないトークン\"{}\"が発見されました。", token.body);
+	}
+	if token.body[0].to_string() != op {
+		exit_eprintln!("\"{}\"を期待した位置で\"{}\"が発見されました。", op, token.body);
+	}
+	// token.toNext();
+}
+
 
 // 期待する次のトークンを(文字列で)指定して読む関数(失敗するとfalseを返す)
-pub fn consume(token: Token, op: String) -> bool {}
+pub fn consume(token: Token, op: String) -> bool {
+	if token.kind != Tokenkind::TK_RESERVED || token.body[0].to_string() != op {
+		false
+	}
+	// token.toNext();
+	true
+}
 
 // EOFかどうかを判断する関数(しばらくは改行文字もEOFとみなす)
-pub fn at_eof(token: Token) -> bool{}
-
-// exitがうまく機能しない時の処理を入れ込むためにマクロを記述
-#[macro_export]
-macro_rules! expect_number {
-	($token:expr) => {
-		match expect_number($token) {
-			Ok(num) => {num}
-			Err(()) => {panic!();}
-		}
-	};
+pub fn at_eof(token: Token) -> bool{
+	token.kind == Tokenkind::TK_EOF
 }
 
-// exitがうまく機能しない時の処理を入れ込むためにマクロを記述
-#[macro_export]
-macro_rules! expect {
-	($op:expr, $token:expr) => {
-		match expect($op, $token) {
-			Ok(()) => {}
-			Err(()) => {panic!();}
-		}
-	};
-}
+
 
