85d01a9e887a4d1908d89f7205a186941f5bc6a9
Yuna Tomida <ytomida.mmm@gmail.com>
2021-07-13 15:53:01 +0900

    トークナイザをBox<Token>を使った方式に書き換え(tokenizer_dev.rs)

---
+++ step4-5/src/main.rs
@@ -0,0 +1,72 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+use anyhow::{Context, Result};
+
+// tokenizerモジュールは未実装
+mod tokenizer;
+mod tokenizer_dev;
+mod parser;
+mod utils;
+mod options;
+use options::Opts;
+use tokenizer::{Token, tokenize, expect, expect_number, consume, at_eof};
+use parser::*;
+
+
+fn main() {
+    // 引数の処理
+    let opts = Opts::parse();
+    
+    // 入力ファイルが指定されているかどうかで分岐
+    if let Some(path) = opts.input_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+		// 1行ごとに処理
+        for line in reader.lines() {
+            // このループ内でlineを式として解釈していく(以降のバージョンではこの部分の変数名はlineに統一する)
+            let line = line.unwrap();
+            let mut asm = ".intel_syntax noprefix\n.globl main\nmain:\n".to_string();
+
+			// よく考えたら参照で渡す意味そんなないかも
+			let token_stream = tokenize(line);
+			let mut lookat: usize = 0;
+
+			// 頭は数字から入ることを想定
+			let num = expect_number(&token_stream, &mut lookat);
+			asm += format!("    mov rax, {}\n", num).as_str();
+
+
+			// EOFまでトークンを処理
+			while !at_eof(&token_stream, &lookat) {
+				if consume(&token_stream, &mut lookat,  "+") {
+					let num = expect_number(&token_stream, &mut lookat);
+					asm += format!("    add rax, {}\n", num).as_str();
+					continue;
+				}
+
+				// +でなければ-を期待して処理
+				expect(&token_stream, &mut lookat,  "-");
+				let num = expect_number(&token_stream, &mut lookat);
+				asm += format!("    sub rax, {}\n", num).as_str();
+
+			}
+
+            
+			// リターン命令を加える
+            asm += format!("    ret").as_str();
+
+
+            // 最後に一気に書き込み
+            println!("{}", asm);
+
+            // 序盤はファイルの最初1行だけを解釈する
+			break;
+        }
+    } else {
+		// fileが指定されていない場合、exit
+		exit_eprintln!("{}{}を指定してください。", "ソース", "ファイル");
+    }
+
+}
