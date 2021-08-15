f6268fff556e824e4b82e4bc722e42f4a1a11572
Yuna Tomida <ytomida.mmm@gmail.com>
2021-08-15 19:43:58 +0900

    アセンブラのプロローグ/エピローグを追加、テストの追加

--- step9/src/main.rs
+++ step9/src/main.rs
@@ -2,8 +2,7 @@ use clap::Clap;
 use std::fs::File;
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::io::{stdin, BufRead, BufReader};
-use anyhow::{Context, Result};
+use std::io::{BufRead, BufReader};
 
 // tokenizerモジュールは未実装
 mod tokenizer;
@@ -21,25 +20,24 @@ fn main() {
     
     // 入力ファイルが指定されているかどうかで分岐
     if let Some(path) = opts.input_file {
-        let f = File::open(path).unwrap();
-        let reader = BufReader::new(f);
-
-		// 改行含め、コード全体を1つの文字列としてトークナイザに入れたい
-		let mut code = "".to_string();
-		for line in reader.lines() {
-			code += format!(" {}", line.unwrap()).as_str();
-		}
-
-		// asmに
-		let mut asm = ".intel_syntax noprefix\n".to_string();
-		asm += ".globl main\n";
-		asm += "main:\n";
+        let f: File = File::open(path).unwrap();
+        let reader: BufReader<File> = BufReader::new(f);
+		let code: String = code_concat(reader);
 
 		// トークナイズしてトークンリストを生成したのち、構文木を生成
 		let mut token_ptr: Rc<RefCell<Token>> = tokenize(code);
-		
 		let node_heads = program(&mut token_ptr);
-		let mut asm = "".to_string();
+
+		// asmにアセンブリを文字列として追加していく
+		let mut asm = ".intel_syntax noprefix\n".to_string();
+		asm += ".globl main\n";
+		asm += "main:\n";
+		
+		// プロローグ(変数の格納領域の確保)
+		asm += "	push rbp\n";
+		asm += "	mov rbp, rsp\n";
+		asm += "	sub rsp, 208\n";
+		
 		// 構文木が複数(stmtの数)生成されているはずなのでそれぞれについて回す
 		for node_ptr in node_heads {
 			// 構文木からコードを生成(asmに追加)
@@ -48,10 +46,10 @@ fn main() {
 			asm += "	pop rax\n";
 		}
 
-		// 結果のpopとリターン命令を追加
-		asm += "	pop rax\n";
+		// エピローグ(リターン処理)
+		asm += "	mov rsp, rbp\n";
+		asm += "	pop rbp\n";
 		asm += "	ret\n";
-
 
 		// 最後に一気に書き込み
 		println!("{}", asm);
@@ -60,5 +58,55 @@ fn main() {
 		// fileが指定されていない場合、exit
 		exit_eprintln!("{}{}を指定してください。", "ソース", "ファイル");
     }
+
+}
+
+
+// 改行含め、コード全体を1つの文字列としてトークナイザに入れたい
+fn code_concat(reader: BufReader<File>) -> String {
+	let mut code = "".to_string();
+	for line in reader.lines() {
+		code += format!(" {}", line.unwrap()).as_str();
+	}
+
+	code
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::code_concat;
+	use std::io::BufReader;
+	use std::fs::File;
+	use std::rc::Rc;
+	use std::cell::RefCell;
+	use crate::tokenizer::{Token, tokenize};
+	use crate::parser::{program};
+
+	#[test]
+	fn code_concat_test() {
+		let path = "./src.txt";
+		let f: File = File::open(path).unwrap();
+        let reader: BufReader<File> = BufReader::new(f);
+		let code: String = code_concat(reader);
+		println!("{}", code);
+	}
+
+	#[test]
+	fn tree_test() {
+		let path = "./3stmt.txt";
+		let f: File = File::open(path).unwrap();
+        let reader: BufReader<File> = BufReader::new(f);
+		let code: String = code_concat(reader);
+		println!("{}", code);
+
+		// トークナイズしてトークンリストを生成したのち、構文木を生成
+		let mut token_ptr: Rc<RefCell<Token>> = tokenize(code);
+		let node_heads = program(&mut token_ptr);
+		println!("trees: {}", node_heads.len());
+		assert_eq!(node_heads.len(), 3);
+
+
+	}
 
 }
