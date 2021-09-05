8c2c58ae621267fc9a90d33a5b306c078e57aecf
Yuna Tomida <ytomida.mmm@gmail.com>
2021-09-06 08:55:39 +0900

    exec-asm.shを変更

--- exec-asm.sh
+++ exec-asm.sh
@@ -1,13 +1,13 @@
 #! /bin/zsh
 
 MOUNT_PATH=$(pwd)
-IMAGE_NAME=$1
-ASM_SRC=$2
+ASM_SRC=$1
 
+# c_execはイメージ名
 docker run --rm \
     -v $MOUNT_PATH:$MOUNT_PATH \
     --platform linux/amd64 \
-    $IMAGE_NAME \
+	c_exec \
     /bin/bash -c \
     "gcc -o ./tmp/tmp $MOUNT_PATH/$ASM_SRC;
     ./tmp/tmp;
