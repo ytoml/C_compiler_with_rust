9f3af71ba000a747f2ad005b1fcad2a7b7458c01
Yuna Tomida <ytomida.mmm@gmail.com>
2021-09-13 23:35:22 +0900

    step13用のディレクトリを作成

---
+++ step13/src/generator.rs
@@ -0,0 +1,362 @@
+use crate::{exit_eprintln};
+use crate::parser::{Node, Nodekind};
+use std::rc::Rc;
+use std::cell::RefCell;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+pub static ASM: Lazy<Mutex<String>> = Lazy::new(
+	|| Mutex::new(
+		".intel_syntax noprefix\n.globl main\n".to_string()
+	)
+);
+
+static CTR_COUNT: Lazy<Mutex<u32>> = Lazy::new(
+	|| Mutex::new(0)
+);
+
+pub fn gen(node: &Rc<RefCell<Node>>) {
+	// 葉にきた、もしくは葉の親のところで左辺値にに何かしらを代入する操作がきた場合の処理
+	match (**node).borrow().kind {
+		Nodekind::NumNd => {
+			// NumNdの時点でunwrapできる
+			*ASM.lock().unwrap() += format!("	push {}\n", (**node).borrow().val.as_ref().unwrap()).as_str();
+			return;
+		},
+		Nodekind::LvarNd => {
+			// 葉、かつローカル変数なので、あらかじめ代入した値へのアクセスを行う
+			gen_lval(node);
+			*ASM.lock().unwrap() += "	pop rax\n"; // gen_lval内で対応する変数のアドレスをスタックにプッシュしているので、popで取れる
+			*ASM.lock().unwrap() += "	mov rax, [rax]\n";
+			*ASM.lock().unwrap() += "	push rax\n";
+			return;
+		},
+		Nodekind::AssignNd => {
+			// 節点、かつアサインゆえ左は左辺値の葉を想定(違えばgen_lval内でエラー)
+			gen_lval((**node).borrow().left.as_ref().unwrap());
+			gen((**node).borrow().right.as_ref().unwrap());
+
+			// 上記gen2つでスタックに変数の値を格納すべきアドレスと、代入する値(式の評価値)がこの順で積んであるはずなので2回popして代入する
+			*ASM.lock().unwrap() += "	pop rdi\n"; 
+			*ASM.lock().unwrap() += "	pop rax\n"; 
+			*ASM.lock().unwrap() += "	mov [rax], rdi\n";
+			*ASM.lock().unwrap() += "	push rdi\n"; // 連続代入可能なように、評価値として代入した値をpushする
+			return;
+		},
+		Nodekind::ReturnNd => {
+			// リターンならleftの値を評価してretする。
+			gen((**node).borrow().left.as_ref().unwrap());
+			*ASM.lock().unwrap() += "	pop rax\n";
+			*ASM.lock().unwrap() += "	mov rsp, rbp\n";
+			*ASM.lock().unwrap() += "	pop rbp\n";
+			*ASM.lock().unwrap() += "	ret\n";
+			return;
+		},
+		Nodekind::IfNd => {
+			// PENDING
+			*CTR_COUNT.lock().unwrap() += 1;
+			let end: String = format!(".LEnd{}", *CTR_COUNT.lock().unwrap());
+
+			// 条件文の処理
+			gen((**node).borrow().enter.as_ref().unwrap());
+			*ASM.lock().unwrap() += "	pop rax\n";
+			*ASM.lock().unwrap() += "	cmp rax, 0\n"; 
+
+			// elseがある場合は微妙にjmp命令の位置が異なることに注意
+			if let Some(ptr) = (**node).borrow().els.as_ref() {
+				let els: String = format!(".LElse{}", *CTR_COUNT.lock().unwrap());
+
+				// falseは0なので、cmp rax, 0が真ならelseに飛ぶ
+				*ASM.lock().unwrap() += format!("je {}\n", els).as_str();
+				gen((**node).borrow().branch.as_ref().unwrap()); // if(true)の場合の処理
+				*ASM.lock().unwrap() += format!("jmp {}\n", end).as_str(); // elseを飛ばしてendへ
+
+				// elseの後ろの処理
+				*ASM.lock().unwrap() += format!("{}:\n", els).as_str();
+				gen(ptr);
+
+			} else {
+				// elseがない場合の処理
+				*ASM.lock().unwrap() += format!("	je {}\n", end).as_str();
+				gen((**node).borrow().branch.as_ref().unwrap());
+			}
+
+			*ASM.lock().unwrap() += format!("{}:\n", end).as_str();
+			return;
+		},
+		Nodekind::WhileNd => {
+			*CTR_COUNT.lock().unwrap() += 1;
+			let begin: String = format!(".LBegin{}", *CTR_COUNT.lock().unwrap());
+			let end: String = format!(".LEnd{}", *CTR_COUNT.lock().unwrap());
+
+			*ASM.lock().unwrap() += format!("{}:\n", begin).as_str();
+			gen((**node).borrow().enter.as_ref().unwrap());
+			*ASM.lock().unwrap() += "	pop rax\n";
+			*ASM.lock().unwrap() += "	cmp rax, 0\n"; // falseは0なので、cmp rax, 0が真ならエンドに飛ぶ
+			*ASM.lock().unwrap() += format!("	je {}\n", end).as_str();
+			
+			gen((**node).borrow().branch.as_ref().unwrap());
+
+			*ASM.lock().unwrap() += format!("	jmp {}\n", begin).as_str();
+
+			*ASM.lock().unwrap() += format!("{}:\n", end).as_str();
+			return;
+		},
+		Nodekind::ForNd => {
+			*CTR_COUNT.lock().unwrap() += 1;
+			let begin: String = format!(".LBegin{}", *CTR_COUNT.lock().unwrap());
+			let end: String = format!(".LEnd{}", *CTR_COUNT.lock().unwrap());
+
+			if let Some(ptr) = (**node).borrow().init.as_ref() {
+				gen(ptr);
+			}
+
+			*ASM.lock().unwrap() += format!("{}:\n", begin).as_str();
+			gen((**node).borrow().enter.as_ref().unwrap());
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+			*ASM.lock().unwrap() += "	cmp rax, 0\n"; // falseは0なので、cmp rax, 0が真ならエンドに飛ぶ
+			*ASM.lock().unwrap() += format!("	je {}\n", end).as_str();
+			
+			gen((**node).borrow().branch.as_ref().unwrap()); // for文内の処理
+			gen((**node).borrow().routine.as_ref().unwrap()); // インクリメントなどの処理
+
+			*ASM.lock().unwrap() += format!("	jmp {}\n", begin).as_str();
+
+			*ASM.lock().unwrap() += format!("{}:\n", end).as_str();
+			return;
+		}, 
+		_ => {}// 他のパターンなら、ここでは何もしない
+	} 
+
+	gen((**node).borrow().left.as_ref().unwrap());
+	gen((**node).borrow().right.as_ref().unwrap());
+
+	*ASM.lock().unwrap() += "	pop rdi\n";
+	*ASM.lock().unwrap() += "	pop rax\n";
+
+	// >, >= についてはオペランド入れ替えのもとsetl, setleを使う
+	match (**node).borrow().kind {
+		Nodekind::AddNd => {
+			*ASM.lock().unwrap() += "	add rax, rdi\n";
+		},
+		Nodekind::SubNd => {
+			*ASM.lock().unwrap() += "	sub rax, rdi\n";
+		},
+		Nodekind::MulNd => {
+			*ASM.lock().unwrap() += "	imul rax, rdi\n";
+		},
+		Nodekind::DivNd  => {
+			*ASM.lock().unwrap() += "	cqo\n";
+			*ASM.lock().unwrap() += "	idiv rdi\n";
+		},
+		Nodekind::EqNd => {
+			*ASM.lock().unwrap() += "	cmp rax, rdi\n";
+			*ASM.lock().unwrap() += "	sete al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		Nodekind::NEqNd => {
+			*ASM.lock().unwrap() += "	cmp rax, rdi\n";
+			*ASM.lock().unwrap() += "	setne al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		Nodekind::LThanNd => {
+			*ASM.lock().unwrap() += "	cmp rax, rdi\n";
+			*ASM.lock().unwrap() += "	setl al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		Nodekind::LEqNd => {
+			*ASM.lock().unwrap() += "	cmp rax, rdi\n";
+			*ASM.lock().unwrap() += "	setle al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		Nodekind::GThanNd => {
+			*ASM.lock().unwrap() += "	cmp rdi, rax\n";
+			*ASM.lock().unwrap() += "	setl al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		Nodekind::GEqNd => {
+			*ASM.lock().unwrap() += "	cmp rdi, rax\n";
+			*ASM.lock().unwrap() += "	setle al\n";
+			*ASM.lock().unwrap() += "	movzb rax, al\n";
+		},
+		_ => {
+			// 上記にないNodekindはここに到達する前にreturnしているはず
+			exit_eprintln!("不正なNodekindです");
+		},
+	}
+
+	*ASM.lock().unwrap() += "	push rax\n";
+
+}
+
+// 正しく左辺値を識別して不正な代入("(a+1)=2;"のような)を防ぐためのジェネレータ関数
+fn gen_lval(node: &Rc<RefCell<Node>>) {
+	if (**node).borrow().kind != Nodekind::LvarNd {
+		exit_eprintln!("代入の左辺値が変数ではありません");
+	}
+
+	// 変数に対応するアドレスをスタックにプッシュする
+	*ASM.lock().unwrap() += "	mov rax, rbp\n";
+	*ASM.lock().unwrap() += format!("	sub rax, {}\n", (**node).borrow().offset.as_ref().unwrap()).as_str();
+	*ASM.lock().unwrap() += "	push rax\n";
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::tokenizer::*;
+	use crate::parser::*;
+
+	static REP:usize = 80;
+
+
+	#[test]
+	fn test_addsub() {
+		println!("test_{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize("1+2+3-1".to_string());
+		let node_ptr = expr(&mut token_ptr);
+		gen(&node_ptr);
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_muldiv() {
+		println!("test_{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize("1+2*3-4/2".to_string());
+		let node_ptr = expr(&mut token_ptr);
+		gen(&node_ptr);
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_brackets() {
+		let equation = "(1+2)/3-1*20".to_string();
+		println!("test_brackets{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_ptr = expr(&mut token_ptr);
+		gen(&node_ptr);
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_unary() {
+		let equation = "(-1+2)*(-1)+(+3)/(+1)".to_string();
+		println!("test_unary{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_ptr = expr(&mut token_ptr);
+		gen(&node_ptr);
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+	
+	#[test]
+	fn test_eq() {
+		let equation = "(-1+2)*(-1)+(+3)/(+1) == 30 + 1".to_string();
+		println!("test_unary{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_ptr = expr(&mut token_ptr);
+		gen(&node_ptr);
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_assign_1() {
+		let equation = "a = 1; a + 1;".to_string();
+		println!("test_assign{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		for node_ptr in node_heads {
+			gen(&node_ptr);
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+		}
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+	#[test]
+	fn test_assign_2() {
+		let equation = "local = 1; local_value = local + 1; local_value99 = local_value + 3;".to_string();
+		println!("test_assign{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		for node_ptr in node_heads {
+			gen(&node_ptr);
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+		}
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_if() {
+		let equation = "
+			i = 10;
+			if (1) i + 1;
+			x = i + 10;
+		".to_string();
+		println!("test_if{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		for node_ptr in node_heads {
+			gen(&node_ptr);
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+		}
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_while() {
+		let equation = "
+			i = 10;
+			while (i > 1) i = i - 1;
+			i;
+		".to_string();
+		println!("test_while{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		for node_ptr in node_heads {
+			gen(&node_ptr);
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+		}
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+
+	#[test]
+	fn test_for() {
+		let equation = "
+			sum = 10;
+			for (i = 0; i < 10; i = i + 1) sum = sum + i;
+			return sum;
+		".to_string();
+		println!("test_for{}", "-".to_string().repeat(REP));
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		for node_ptr in node_heads {
+			gen(&node_ptr);
+
+			*ASM.lock().unwrap() += "	pop rax\n";
+		}
+
+		println!("{}", ASM.lock().unwrap());
+
+	}
+}
---
+++ step13/src/parser.rs
@@ -0,0 +1,542 @@
+// 再帰下降構文のパーサ
+use crate::tokenizer::{Token, Tokenkind, consume, consume_kind, expect, expect_number, expect_ident, is_ident, at_eof};
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+use std::fmt::{Formatter, Display, Result};
+
+static LOCALS: Lazy<Mutex<HashMap<String, usize>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+pub static LVAR_MAX_OFFSET: Lazy<Mutex<usize>> = Lazy::new(|| Mutex::new(0));
+
+#[derive(Debug, PartialEq)]
+pub enum Nodekind {
+	DefaultNd, // defalut
+	AddNd, // '+'
+	SubNd, // '-'
+	MulNd, // '*'
+	DivNd, // '/'
+	AssignNd, // '='
+	LvarNd, // 左辺値
+	NumNd, // 数値
+	EqNd, // "=="
+	NEqNd, // "!="
+	GThanNd, // '>'
+	GEqNd, // ">="
+	LThanNd, // '<'
+	LEqNd, // "<="
+	IfNd, // if
+	// ElseNd, // else
+	ForNd, // for
+	WhileNd, // while
+	ReturnNd, // return
+}
+
+pub struct Node {
+	pub kind: Nodekind, // Nodeの種類
+
+	// プロパティとなる数値
+	pub val: Option<i32>,
+	pub offset: Option<usize>,// ベースポインタからのオフセット(ローカル変数時のみ)
+
+	// 通常ノード(計算式評価)用の左右ノード
+	pub left: Option<Rc<RefCell<Node>>>,
+	pub right: Option<Rc<RefCell<Node>>>,
+
+	// for (init; enter; routine) branch, if (enter) branch else els, while(enter) branch 
+	pub init: Option<Rc<RefCell<Node>>>,
+	pub enter: Option<Rc<RefCell<Node>>>, 
+	pub routine: Option<Rc<RefCell<Node>>>, 
+	pub branch: Option<Rc<RefCell<Node>>>,
+	pub els: Option<Rc<RefCell<Node>>>,
+
+}
+
+// 初期化を簡単にするためにデフォルトを定義
+impl Default for Node {
+	fn default() -> Node {
+		Node { kind: Nodekind::DefaultNd, val: None, offset: None, left: None, right: None, init: None, enter: None, routine: None, branch: None, els: None, }
+	}
+}
+
+static REP_NODE:usize = 40;
+impl Display for Node {
+	fn fmt(&self, f:&mut Formatter) -> Result {
+
+		let mut s = format!("{}\n", "-".to_string().repeat(REP_NODE));
+		s = format!("{}Nodekind : {:?}\n", s, self.kind);
+
+		if let Some(e) = self.val.as_ref() {
+			s = format!("{}val: {}\n", s, e);
+		}
+		
+		if let Some(e) = self.offset.as_ref() {
+			s = format!("{}offset: {}\n", s, e);
+		} 
+
+		if let Some(e) = self.left.as_ref() {
+			s = format!("{}left: exist(kind:{:?})\n", s, e.borrow().kind);
+		} 
+
+		if let Some(e) = self.right.as_ref() {
+			s = format!("{}right: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		if let Some(e) = self.init.as_ref() {
+			s = format!("{}init: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		if let Some(e) = self.enter.as_ref() {
+			s = format!("{}enter: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		if let Some(e) = self.routine.as_ref() {
+			s = format!("{}routine: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		if let Some(e) = self.branch.as_ref() {
+			s = format!("{}branch: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		if let Some(e) = self.els.as_ref() {
+			s = format!("{}els: exist(kind:{:?})\n", s, e.borrow().kind);
+		}
+
+		write!(f, "{}", s)
+	}
+}
+
+
+// ノードの作成
+fn new_node_calc(kind: Nodekind, left: Rc<RefCell<Node>>, right: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
+	Rc::new(RefCell::new(
+		Node {
+			kind: kind,
+			left: Some(left), 
+			right: Some(right),
+			.. Default::default()
+		}
+	))
+}
+
+// 数字に対応するノード
+fn new_node_num(val: i32) -> Rc<RefCell<Node>> {
+	Rc::new(RefCell::new(
+		Node {
+			kind: Nodekind::NumNd,
+			val: Some(val),
+			.. Default::default()
+		}
+	))
+}
+
+// 左辺値(今のうちはローカル変数)に対応するノード(現在は1文字のみ)
+fn new_node_lvar(name: impl Into<String>) -> Rc<RefCell<Node>> {
+	let name: String = name.into();
+	let offset;
+
+
+	// デッドロック回避のため、フラグを用意してmatch内で再度LOCALS(<変数名, オフセット>のHashMap)にアクセスしないようにする
+	let mut not_found: bool = false;
+	match LOCALS.lock().unwrap().get(&name) {
+		Some(_offset) => {
+			offset = *_offset;
+		}, 
+		// 見つからなければオフセットの最大値を伸ばす
+		None => {
+			*LVAR_MAX_OFFSET.lock().unwrap() += 8; 
+			offset = *LVAR_MAX_OFFSET.lock().unwrap();
+			not_found = true;
+		}
+	}
+
+	if not_found {
+		LOCALS.lock().unwrap().insert(name, offset); 
+	}
+	
+	Rc::new(RefCell::new(
+		Node {
+			kind: Nodekind::LvarNd,
+			offset: Some(offset),
+			.. Default::default()
+		}
+
+	))
+}
+
+
+// 生成規則: program = stmt*
+pub fn program(token_ptr: &mut Rc<RefCell<Token>>) -> Vec<Rc<RefCell<Node>>> {
+	let mut statements :Vec<Rc<RefCell<Node>>> = Vec::new();
+	while !at_eof(token_ptr) {
+		statements.push(stmt(token_ptr));
+	}
+
+	statements
+}
+
+
+// 生成規則: stmt = 
+// expr? ";"
+// "if" "(" expr ")" stmt ("else" stmt)? ; 今はelse ifは実装しない
+// "while" "(" expr ")" stmt
+// "for" "(" expr? ";" expr? ";" expr? ")" stmt 
+// "return" expr ";"
+// まだブロックには対応していない(一気に実装してごちゃつくのを防ぐため
+fn stmt(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let node_ptr: Rc<RefCell<Node>>;
+
+	// if consume(token_ptr, ";") {return node_ptr;}
+
+	if consume(token_ptr, "if") {
+		expect(token_ptr, "(");
+		let enter= Some(expr(token_ptr));
+
+		expect(token_ptr, ")");
+		let branch = Some(stmt(token_ptr));
+
+		if consume(token_ptr, "else") {
+			let els = Some(stmt(token_ptr));
+			node_ptr = Rc::new(RefCell::new(
+			Node {
+				kind: Nodekind::IfNd,
+				enter: enter,
+				branch: branch,
+				els: els,
+				..Default::default()
+				}
+			));
+		} else {
+			node_ptr = Rc::new(RefCell::new(
+			Node {
+				kind: Nodekind::IfNd,
+				enter: enter,
+				branch: branch,
+				..Default::default()
+				}
+			));
+		}
+		
+
+	} else if consume(token_ptr, "while") {
+		expect(token_ptr, "(");
+		let enter = Some(expr(token_ptr));
+		expect(token_ptr, ")");
+
+		let branch = Some(stmt(token_ptr));
+
+		node_ptr = Rc::new(RefCell::new(
+			Node {
+				kind: Nodekind::WhileNd,
+				enter: enter,
+				branch: branch,
+				..Default::default()
+			}
+		));
+
+	} else if consume(token_ptr, "for") {
+		expect(token_ptr, "(");
+
+		let init: Option<Rc<RefCell<Node>>>;
+		let enter: Option<Rc<RefCell<Node>>>;
+		let routine: Option<Rc<RefCell<Node>>>;
+		let branch: Option<Rc<RefCell<Node>>>;
+
+		if consume(token_ptr, ";") {
+			// consumeできた場合exprが何も書かれていないことに注意
+			init = None;
+		} else {
+			init = Some(expr(token_ptr));
+			expect(token_ptr, ";");
+		}
+
+		// concumeの条件分岐について同上なので注意
+		if consume(token_ptr, ";") {
+			enter = None;
+		} else {
+			enter = Some(expr(token_ptr));
+			expect(token_ptr, ";")
+		}
+
+		if consume(token_ptr, ")") {
+			routine = None;
+		} else {
+			routine = Some(expr(token_ptr));
+			expect(token_ptr, ")")
+		}
+
+		branch = Some(stmt(token_ptr));
+		
+		node_ptr = Rc::new(RefCell::new(
+			Node {
+				kind: Nodekind::ForNd,
+				init: init,
+				enter: enter,
+				routine: routine,
+				branch: branch,
+				..Default::default()
+			}
+		));
+
+	} else if consume_kind(token_ptr, Tokenkind::ReturnTk) {
+		// ReturnNdはここでしか生成しないため、ここにハードコードする
+		node_ptr = Rc::new(RefCell::new(
+			Node {
+				kind: Nodekind::ReturnNd,
+				left: Some(expr(token_ptr)),
+				..Default::default()
+			}
+		));
+		expect(token_ptr, ";");
+
+	} else {
+		node_ptr = expr(token_ptr);
+		expect(token_ptr, ";");
+
+	}
+
+	node_ptr
+}
+
+// 生成規則: expr = assign
+pub fn expr(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	assign(token_ptr)
+}
+
+// 生成規則: assign = equality ("=" assign)?
+fn assign(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+
+	let mut node_ptr = equality(token_ptr);
+	if consume(token_ptr, "=") {
+		node_ptr = new_node_calc(Nodekind::AssignNd, node_ptr,  assign(token_ptr));
+	}
+	
+	node_ptr
+}
+
+// 生成規則: equality = relational ("==" relational | "!=" relational)?
+pub fn equality(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+
+	let mut node_ptr = relational(token_ptr);
+	if consume(token_ptr, "==") {
+		node_ptr = new_node_calc(Nodekind::EqNd, node_ptr, relational(token_ptr));
+
+	} else if consume(token_ptr, "!=") {
+		node_ptr = new_node_calc(Nodekind::NEqNd, node_ptr, relational(token_ptr));
+	}
+
+	node_ptr
+}
+
+// 生成規則: relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+fn relational(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let mut node_ptr = add(token_ptr);
+
+	loop {
+		if consume(token_ptr, "<") {
+			node_ptr = new_node_calc(Nodekind::LThanNd, node_ptr, add(token_ptr));
+
+		} else if consume(token_ptr, "<=") {
+			node_ptr = new_node_calc(Nodekind::LEqNd, node_ptr, add(token_ptr));
+
+		} else if consume(token_ptr, ">") {
+			node_ptr = new_node_calc(Nodekind::GThanNd, node_ptr, add(token_ptr));
+
+		} else if consume(token_ptr, ">=") {
+			node_ptr = new_node_calc(Nodekind::GEqNd, node_ptr, add(token_ptr));
+
+		} else{
+			break;
+		}
+	}
+
+
+	node_ptr
+
+}
+
+// 生成規則: add = mul ("+" mul | "-" mul)*
+pub fn add(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let mut node_ptr = mul(token_ptr);
+
+	loop {
+		if consume(token_ptr, "+") {
+			node_ptr = new_node_calc(Nodekind::AddNd, node_ptr, mul(token_ptr));
+
+		} else if consume(token_ptr, "-") {
+			node_ptr = new_node_calc(Nodekind::SubNd, node_ptr, mul(token_ptr));
+
+		} else {
+			break;
+		}
+	}
+
+	node_ptr
+}
+
+// 生成規則: mul = unary ("*" unary | "/" unary)*
+fn mul(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let mut node_ptr = unary(token_ptr);
+
+	loop {
+		if consume(token_ptr, "*") {
+			node_ptr = new_node_calc(Nodekind::MulNd, node_ptr, unary(token_ptr));
+
+		} else if consume(token_ptr, "/") {
+			node_ptr = new_node_calc(Nodekind::DivNd, node_ptr, unary(token_ptr));
+
+		} else {
+			break;
+		}
+	}
+
+	node_ptr
+}
+
+
+// 生成規則: unary = ("+" | "-")? primary
+fn unary(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let node_ptr;
+
+	if consume(token_ptr, "+") {
+		node_ptr = primary(token_ptr);
+
+	} else if consume(token_ptr, "-") {
+		// 単項演算のマイナスは0から引く形にする。
+		node_ptr = new_node_calc(Nodekind::SubNd, new_node_num(0), primary(token_ptr));
+
+	} else {
+		node_ptr = primary(token_ptr);
+	}
+
+	node_ptr
+}
+
+
+
+// 生成規則: primary = num | ident | "(" expr ")"
+fn primary(token_ptr: &mut Rc<RefCell<Token>>) -> Rc<RefCell<Node>> {
+	let node_ptr;
+
+	if consume(token_ptr, "(") {
+		node_ptr = expr(token_ptr);
+
+		expect(token_ptr, ")");
+
+	} else if is_ident(token_ptr) {
+		node_ptr = new_node_lvar(expect_ident(token_ptr));
+
+	} else {
+		node_ptr = new_node_num(expect_number(token_ptr));
+
+	}
+
+	node_ptr
+}
+
+mod tests {
+	use super::*;
+	use crate::tokenizer::tokenize;
+
+	static REP: usize = 40;
+
+	fn search_tree(tree: &Rc<RefCell<Node>>) {
+		let node: &Node = &*(*tree).borrow();
+		println!("{}", node);
+
+		if node.left.is_some() {search_tree(node.left.as_ref().unwrap());}
+		if node.right.is_some() {search_tree(node.right.as_ref().unwrap());}
+		if node.init.is_some() {search_tree(node.init.as_ref().unwrap());}
+		if node.enter.is_some() {search_tree(node.enter.as_ref().unwrap());}
+		if node.routine.is_some() {search_tree(node.routine.as_ref().unwrap());}
+		if node.branch.is_some() {search_tree(node.branch.as_ref().unwrap());}
+		if node.els.is_some() {search_tree(node.els.as_ref().unwrap());}
+	}
+
+
+	#[test]
+	fn test_display() {
+		println!("test_display{}", "-".to_string().repeat(REP));
+		let node = new_node_num(0);
+		println!("{}", (*node).borrow());
+	}
+
+
+	#[test]
+	fn test_for() {
+		println!("test_for{}", "-".to_string().repeat(REP));
+		let equation = "
+			sum = 10;
+			sum = sum + i;
+			for (i = 1 ; i < 10; i = i + 1) sum = sum +i;
+			sum;
+		".to_string();
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		let mut count: usize = 1;
+		for node_ptr in node_heads {
+			println!("stmt{}{}", count, "-".to_string().repeat(REP));
+			search_tree(&node_ptr);
+			count += 1;
+		}
+	}
+
+	#[test]
+	fn test_while() {
+		println!("test_while{}", "-".to_string().repeat(REP));
+		let equation = "
+			sum = 10;
+			while(sum > 0) sum = sum - 1;
+			sum;
+		".to_string();
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		let mut count: usize = 1;
+		for node_ptr in node_heads {
+			println!("stmt{}{}", count, "-".to_string().repeat(REP));
+			search_tree(&node_ptr);
+			count += 1;
+		}
+	}
+
+	#[test]
+	fn test_if() {
+		println!("test_while{}", "-".to_string().repeat(REP));
+		let equation = "
+			i = 10;
+			if (i == 10) i = i / 5;
+			if (i == 2) i = i + 5; else i = i / 5;
+			i;
+		".to_string();
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		let mut count: usize = 1;
+		for node_ptr in node_heads {
+			println!("stmt{}{}", count, "-".to_string().repeat(REP));
+			search_tree(&node_ptr);
+			count += 1;
+		}
+	}
+
+
+	#[test]
+	fn test_combination() {
+		println!("test_combination{}", "-".to_string().repeat(REP));
+		let equation = "
+			i = 10;
+			if (i == 10) i = i / 5;
+			if (i == 2) i = i + 5; else i = i / 5;
+			i;
+		".to_string();
+		let mut token_ptr = tokenize(equation);
+		let node_heads = program(&mut token_ptr);
+		let mut count: usize = 1;
+		for node_ptr in node_heads {
+			println!("stmt{}{}", count, "-".to_string().repeat(REP));
+			search_tree(&node_ptr);
+			count += 1;
+		} 
+	}
+
+}
