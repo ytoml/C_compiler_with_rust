b074d3db0b68c25f973edff284c599d5e05b2d04
Yuna Tomida <ytomida.mmm@gmail.com>
2021-10-28 20:20:35 +0900

    typecell: TypeCell への PartialEq トレイトの実装

--- step18/src/typecell.rs
+++ step18/src/typecell.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use std::fmt::{Display, Formatter};
 use std::fmt;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Type {
 	Invalid, // デフォルトや無名ノードに割り当てる
 	Int,
@@ -22,7 +22,7 @@ impl Display for Type {
 	}
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, Eq)] // PartialEq は別で実装
 pub struct TypeCell {
 	pub typ: Type,
 	pub ptr_to: Option<Rc<RefCell<TypeCell>>>,
@@ -32,6 +32,16 @@ impl TypeCell {
 	pub fn new(typ: Type) -> Self {
 		TypeCell { typ:typ, ptr_to: None }
 	}
+
+	fn get_ptr_chains(&self) -> (usize, Type) {
+		match self.ptr_to.as_ref() {
+			Some(ptr) => {
+				let (p, typ) = ptr.borrow_mut().get_ptr_chains();
+				(p+1, typ)
+			}
+			None => (0, self.typ.clone())
+		}
+	}
 }
 
 impl Default for TypeCell {
@@ -40,5 +50,40 @@ impl Default for TypeCell {
 	}
 }
 
+impl PartialEq for TypeCell {
+	// ポインタが連なっている個数と、最終的に指されている型が両方同じ時にイコールとみなす
+	fn eq(&self, other: &Self) -> bool {
+		self.get_ptr_chains() == other.get_ptr_chains()
+	}
+}
+
 unsafe impl Send for TypeCell {}
 unsafe impl Sync for TypeCell {}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn typecell_eq() {
+		let mut t1 = TypeCell::new(Type::Int);
+		let mut t2 = TypeCell::new(Type::Int);
+		assert_eq!(t1, t2);
+
+		for _ in 0..10 {
+			let mut ptr =  TypeCell::new(Type::Ptr);
+			let _ = ptr.ptr_to.insert(Rc::new(RefCell::new(t1)));
+			t1 = ptr;
+		}
+
+		for _ in 0..11 {
+			let mut ptr =  TypeCell::new(Type::Ptr);
+			let _ = ptr.ptr_to.insert(Rc::new(RefCell::new(t2)));
+			t2 = ptr;
+		}
+
+		assert_ne!(t1, t2);
+		assert_eq!(t1, *t2.ptr_to.as_ref().unwrap().as_ref().borrow());
+	}
+
+}
